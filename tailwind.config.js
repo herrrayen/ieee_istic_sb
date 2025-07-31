027c95cc485d868e6305cd21766289c36be9be8e
HerrRayen <dabrantube@gmail.com>
2025-07-31 10:00:49 +0100

    fixed snippet

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,23 +29,20 @@ const config = {
     },
   },
   darkMode: "class",
-  plugins: [
-    heroui({themes: {
-      light: {
-        colors: {
-          primary: {
-            DEFAULT: "#005BC4",
-            foreground: "#000000",
-          },
-          secondary:{
-            DEFAULT: "#FFFFFF ",
-          },
-          focus: "#BEF264",
+  plugins: [heroui({themes: {
+    light: {
+      colors: {
+        primary: {
+          DEFAULT: "#005BC4",
+          foreground: "#000000",
         },
+        secondary:{
+          DEFAULT: "#FFFFFF ",
+        },
+        focus: "#BEF264",
       },
-    }}),
-    require('tailwind-scrollbar-hide')
-  ],
+    },
+  },})],  
 }
 
 module.exports = withMT(config);

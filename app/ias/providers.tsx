4e275399925e6ff0083e273a82a02b4475447689
HerrRayen <dabrantube@gmail.com>
2025-08-08 15:33:30 +0100

    fix: update provider logic in app/ias/providers.tsx

--- app/ias/providers.tsx
+++ app/ias/providers.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import type { ThemeProviderProps } from "next-themes";
 import { useRouter } from "next/navigation";
 
 export interface ProvidersProps {
